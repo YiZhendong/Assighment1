c77359aa5b324650de1c8742a54d473e8c5bfe9f
S <s>
2016-06-27 23:16:51 +0800

    {"types":"C","runs":9}

--- MidpointFindingKarel.java
+++ MidpointFindingKarel.java
@@ -54,13 +54,13 @@ public class MidpointFindingKarel extends SuperKarel {
 
 	private void removeWestBlock() {
 		turnAround();
-		while(beepersPresent()){
+		/*while(beepersPresent()){
 			move();
 		}
 		turnAround();
 		move();
 		pickBeeper();
-		
+		*/
 	}
 
 }

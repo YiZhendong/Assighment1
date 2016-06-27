6d3686594bdf88f222db456192d01ff6e303b4ae
S <s>
2016-06-27 16:05:10 +0800

    {"types":"C","runs":10}

--- CollectNewspaperKarel.java
+++ CollectNewspaperKarel.java
@@ -10,7 +10,7 @@
 
 import stanford.karel.*;
 
-public class CollectNewspaperKarel extends SuperKarel {
+public class CollectNewspaperKarel extends superKarel {
 	public void run(){
 		move();
 		move();

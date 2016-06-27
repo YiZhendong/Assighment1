b6e891a438ae3ca959f292d823b3b0dc412797b3
S <s>
2016-06-27 23:37:20 +0800

    {"types":"C","runs":22}

--- MidpointFindingKarel.java
+++ MidpointFindingKarel.java
@@ -34,6 +34,8 @@ public class MidpointFindingKarel extends SuperKarel {
 
 	// 循环清除左右两边的方块
 	private void removeBlocksStepByStep() {
+		removeEastBlock();					//清除最东边的方块
+		removeWestBlock();					//接着清除最西边的方块
 		while(beepersPresent()){
 			move();
 			if(beepersPresent()){

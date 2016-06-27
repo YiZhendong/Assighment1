8d754e0f6394859fb24c63f9b1a536826568d89f
S <s>
2016-06-27 20:39:29 +0800

    {"types":"C","runs":5}

--- CollectNewspaperKarel.java
+++ CollectNewspaperKarel.java
@@ -6,12 +6,63 @@
  * instruct Karel to walk to the door of its house, pick up the
  * newspaper (represented by a beeper, of course), and then return
  * to its initial position in the upper left corner of the house.
+ * 
+ * author:zhendongYi
  */
 
 import stanford.karel.*;
 
-public class CollectNewspaperKarel extends Karel {
+public class CollectNewspaperKarel extends SuperKarel {
 	
-	// You fill in this part
+	/*
+	 * 主方法
+	 * @see stanford.karel.SuperKarel#run()
+	 */
+	public void run(){
+		CollectNewspaper();		//取报纸
+		GoBack();				//返回
+	}
+
+	/*
+	 * 取报纸
+	 */
+	private void CollectNewspaper() {
+		GoWithFloor();			//沿着天花板走
+		GoWithWall();			//沿着墙走
+		PickNewspaper();		//捡报纸
+	}
+
+	/*
+	 * 沿着墙走
+	 */
+	private void GoWithWall() {
+		while(leftIsBlocked()){		//前进
+			move();
+		}
+		turnLeft();					//左转
+	}
+
+	/*
+	 * 沿着天花板走
+	 */
+	private void GoWithFloor() {
+		while(frontIsBlocked()){	//前进
+			move();
+		}
+		turnRight();				//右转
+	}
+
+	private void PickNewspaper() {
+		move();
+		pickBeeper();
+	}
+	
+	private void GoBack() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
 
 }

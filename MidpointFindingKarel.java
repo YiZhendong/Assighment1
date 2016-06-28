a7617aef070683e94fbd3bf9ee285781ec10e8cc
S <s>
2016-06-28 15:24:20 +0800

    {"types":"C","runs":8}

--- MidpointFindingKarel.java
+++ MidpointFindingKarel.java
@@ -40,7 +40,7 @@ public class MidpointFindingKarel extends SuperKarel {
 			/*moveToBlockEnd();
 			removeEastBlock();
 			moveToBlockEnd();
-			removeWestBlock();	*/
+			removeWestBlock();*/	
 		}
 	}
 	

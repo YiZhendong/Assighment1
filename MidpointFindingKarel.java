769c65bea1d3f191ba08057e30b1ddadb32ec262
S <s>
2016-06-28 10:54:43 +0800

    {"types":"C","runs":5}

--- MidpointFindingKarel.java
+++ MidpointFindingKarel.java
@@ -83,18 +83,8 @@ public class MidpointFindingKarel extends SuperKarel {
 
 	//移除最西边的方块
 	private void removeWestBlock() {		
-		turnAround();						//转身
-		move();								//move一格
-		while(beepersPresent()){			//如果当先存在方块
-			if(frontIsClear()){				//并且前方的格子中没有被阻塞
-				move();
-			}else{
-				break;
-			}
-		}
-		turnAround();
 		pickBeeper();
-		move();
+		turnAround();
 	}
 
 }
